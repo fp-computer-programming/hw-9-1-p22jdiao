00b6c982cf58528fcf7691957aa120046d7b8aa5
p22jdiao <p22jdiao@fairfieldprep.org>
2022-01-12 09:42:53 -0500

    finished

--- hw9-1-3.py
+++ hw9-1-3.py
@@ -2,10 +2,10 @@
 
 def find_thing(working_item, search_item):
     for i,v in enumerate(working_item):
-        if search_item == v:
+        if v == search_item:
             return i
 
-        return -1
+    return -1
 
-x = find_thing([0,1,2,3,4,5,6],3)
+x = find_thing([0,1,2,3,4,5,6],7)
 print(x)
