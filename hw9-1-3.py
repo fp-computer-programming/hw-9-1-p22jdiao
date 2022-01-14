d1f32b68ca9ae5d78eed4b269909bf76d250efb6
p22jdiao <p22jdiao@fairfieldprep.org>
2022-01-14 10:15:41 -0500

    changed-finished

--- hw9-1-3.py
+++ hw9-1-3.py
@@ -2,10 +2,16 @@
 
 def find_thing(working_item, search_item):
     for i,v in enumerate(working_item):
-        if v == search_item:
-            return i
+        if v == search_item[0]:
+            mark = i
+            check = ""
+            for x in range(len(search_item)):
+                check += working_item[mark]
+                mark += 1
+            if check == search_item:
+                return i
 
     return -1
 
-x = find_thing([0,1,2,3,4,5,6],7)
+x = find_thing("apple", "ple")
 print(x)
